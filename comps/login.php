36fadd22fcc933f869401de7e91f207fb4a5f21a
Flexo874 <felixcapelgoudard@gmail.com>
2024-07-17 11:58:00 +0200

    dsfd

--- comps/login.php
+++ comps/login.php
@@ -1,9 +1,10 @@
 <?php
+include_once 'pages/connect.php';
 
 session_start();
 #https://www.php.net/manual/fr/function.header.php
 if (!isset($_SESSION['username'])) {
-    header('location: /pages/connect.php');
+    header('location: pages/connect.php');
 
 }
 
