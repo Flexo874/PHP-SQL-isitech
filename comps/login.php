a5b5643ac231cbca5ef4532499c3ab3adad61caf
Flexo874 <felixcapelgoudard@gmail.com>
2024-07-17 11:47:08 +0200

    st

--- comps/login.php
+++ comps/login.php
@@ -3,7 +3,6 @@
 session_start();
 #https://www.php.net/manual/fr/function.header.php
 if (!isset($_SESSION['username'])) {
-    header('location: ../pages/connect.php');
-
+    header('location: /var/html/PHP-SQL-isitech/pages/connect.php');
 }
 
