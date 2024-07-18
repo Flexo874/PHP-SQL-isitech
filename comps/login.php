ee37eba5f1fc3126534b847d84ac3ee0aaebc4b7
Flexo874 <felixcapelgoudard@gmail.com>
2024-07-18 09:58:49 +0200

    Update login.php

--- comps/login.php
+++ comps/login.php
@@ -3,7 +3,7 @@
 session_start();
 #https://www.php.net/manual/fr/function.header.php
 if (!isset($_SESSION['username'])) {
-    header('location: ./pages/connect.php');
+    header('location: ../pages/connect.php');
 
 }
 
